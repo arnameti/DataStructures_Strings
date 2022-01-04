6f0ed2f81f44fa415160a5bc17b5cfe216291ec4
arnameti <arnameti@gmail.com>
2022-01-04 08:55:35 +0100

    Strings 8kyu - challange_1 - return a suit

--- 8 kyu/script.js
+++ 8 kyu/script.js
@@ -1,2 +1,27 @@
-"use strict"
+"use strict";
 
+// 1) return a suit
+// You get any card as an argument. Your task is to return a suit of this card.
+
+// ('3♣') -> return 'clubs'
+// ('3♦') -> return 'diamonds'
+// ('3♥') -> return 'hearts'
+// ('3♠') -> return 'spades'
+// -----------------------------------------------------------
+
+const whichSuit = function (card) {
+  const s = {
+    "♣": "clubs",
+    "♦": "diamonds",
+    "♥": "hearts",
+    "♠": "spades",
+  };
+
+  // return s[card.charAt(card.length -1)]
+  // return s[card[card.length -1]]
+  return s[card.slice(1)]
+};
+
+console.log(
+  whichSuit('3♣')
+);
