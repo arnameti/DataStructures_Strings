e793b2c9264fc339c89396fac1f14b043623e382
arnameti <arnameti@gmail.com>
2022-01-14 09:10:26 +0100

    Strings 8 kuy - challenge_15 - remove char

--- 8 kyu/script.js
+++ 8 kyu/script.js
@@ -282,6 +282,7 @@ console.log(convertName2("Arnor Ameti"));
 // 'word'   =>  'drow'
 // -----------------------------------------------------------
 
+/*
 const reverseString = function (string) {
   let arr = [];
   for (let i = string.length - 1; i >= 0; i--) {
@@ -310,6 +311,7 @@ console.log(reverseString3("hallo"));
 
 const reverseString4 = (str) => str.split("").reverse().join("");
 console.log(reverseString4("hallo"));
+// --------------------
 
 const reverseString5 = function(str) {
   if (str === "")
@@ -325,4 +327,19 @@ const reverseString6 = function(str) {
 }
 console.log(reverseString6("hallo"));
 // --------------------
+*/
+
+// ###############################################################################################
+// 15)
+// It's pretty straightforward. Your goal is to create a function that removes the first and last characters of a string. You're given one parameter, the original string. You don't have to worry with strings with less than two characters.
+// -----------------------------------------------------------
+
+const removeChar = function (string) {
+  return string.slice(1, -1);
+};
+console.log(removeChar('hallo'));
+
+const removeChar2 = str => str.slice(1, -1);
+console.log(removeChar2('hallo'));
+
 
